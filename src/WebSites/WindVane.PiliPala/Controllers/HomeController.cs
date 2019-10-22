2fa108b689b3135c5e26b237950069b2522ab53a
PseudoJunZi <PseudoJunZi@163.com>
2019-10-23 07:50:23 +0800

    Update Pages

--- src/WebSites/WindVane.PiliPala/Controllers/HomeController.cs
+++ src/WebSites/WindVane.PiliPala/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace WindVane.PiliPala.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
